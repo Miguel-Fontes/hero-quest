a1abbebfb9346e837d700bc420f9ffa18accd1ff
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-06-19 22:17:00 -0300

    Utils directory with aliases script

---
+++ utils/aliases.sh
@@ -0,0 +1,2 @@
+#bin/bash
+alias test='java -jar /opt/junit5/console/junit-platform-console-standalone-1.2.0-RC1.jar --cp ./target/test-classes/:./target/classes/:./target/dependency/essentials-validation-1.0.jar --select-package "br.com.miguelmf"'
