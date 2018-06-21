b81ff7dc26d60edb3b2c3d1c52f88b30973277aa
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-06-20 23:13:23 -0300

    Improvements to comply with acceptance criteria

--- src/test/java/br/com/miguelmf/heroquest/HeroQuestTest.java
+++ src/test/java/br/com/miguelmf/heroquest/HeroQuestTest.java
@@ -1,21 +1,15 @@
 package br.com.miguelmf.heroquest;
 
-import br.com.miguelmf.heroquest.core.actions.BasicAttackAction;
-import br.com.miguelmf.heroquest.core.battle.Battle;
-import br.com.miguelmf.heroquest.core.hero.ActionStub;
-import br.com.miguelmf.heroquest.core.hero.Hero;
-import br.com.miguelmf.heroquest.core.hero.HeroTestFactory;
-import br.com.miguelmf.heroquest.core.hero.HeroType;
-import br.com.miguelmf.heroquest.core.hero.SelectorStub;
-import br.com.miguelmf.heroquest.core.selectors.BasicAttackSelector;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import br.com.miguelmf.heroquest.core.actions.BasicAttackAction;
+import br.com.miguelmf.heroquest.core.battle.Battle;
+import br.com.miguelmf.heroquest.core.hero.Hero;
+import br.com.miguelmf.heroquest.core.hero.HeroTestFactory;
+import br.com.miguelmf.heroquest.core.selectors.BasicAttackSelector;
 
 @DisplayName("Hero Quest")
 class HeroQuestTest {
@@ -29,8 +23,10 @@ class HeroQuestTest {
 
         Battle battle = Battle.of(smoothHero, brutalHero);
 
-        while (!battle.isComplete()) {
+        int maxIterations = 100;
+        while (!battle.isComplete() && maxIterations > 0) {
             battle = battle.nextTurn();
+            maxIterations--;
         }
 
         Assertions.assertTrue(battle.getWinner().isPresent(), "The battle is done but there is no Winner");
@@ -39,7 +35,8 @@ class HeroQuestTest {
 	private Hero buildRandomHero() {
         return HeroTestFactory.newInstance().getBuilder()
             .addAction(BasicAttackAction.newInstance())
-            .selector(BasicAttackSelector.newInstance()).build();
+            .selector(BasicAttackSelector.newInstance())
+            .build();
 	}
 
 }
