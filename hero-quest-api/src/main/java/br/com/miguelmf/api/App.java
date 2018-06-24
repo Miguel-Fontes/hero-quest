8f35ee794e83bcc47ce886a7afa8772afa87d293
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-06-24 19:20:07 -0300

    Removal of unneeded files

--- hero-quest-api/src/main/java/br/com/miguelmf/api/App.java
+++
@@ -1,8 +0,0 @@
-package br.com.miguelmf.api;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- hero-quest-ports/src/main/java/br/com/miguelmf/heroquest/App.java
+++
@@ -1,8 +0,0 @@
-package br.com.miguelmf.heroquest;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
