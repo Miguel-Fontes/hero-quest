30bbc67173b43c83412498653ce52719f8460143
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-06-26 08:16:55 -0300

    New version of module.sh

--- utils/module.sh
+++ utils/module.sh
@@ -4,30 +4,34 @@
 # Creates maven modules based on a project.
 #
 # Changelog
-# Version 0: Hardcoded group id, package data, archetype and others. Not ready for general use.
+# Version 0: - Hardcoded group id, package data, archetype and others. Not ready for general use.
+# Version 1: - Fetches the Group Id from pom.xml.
+#            - Constans are now read-only (added -r switch)
 #
 # Future Features
-# - Force confirmation switch "-y"
-# - Read project information from pom.xml, and change package, group id and other parameters accordingly
-# - Parameters that constants for a user (my preferred default group id) should be externalized in some sort of configuration
+# - [ ] Force confirmation switch "-y"
+# - [ ] Read project information from pom.xml, and change package, group id and other parameters accordingly
+# - [ ] Parameters that constants for a user (my preferred default group id) should be externalized in some sort of configuration
+# - [ ] Add option to enter the group id manually, if it is desired
+# - [ ] Validate if working directory has a pom.xml, otherwise stop execution with a discriptive message
 #
 # Miguel Fontes, 06/2018
 
 # Command Line Parameters
-declare ARTIFACT_ID=$1
-declare PACKAGE_MODIFIER=$2
+declare -r ARTIFACT_ID=$1
+declare -r PACKAGE_MODIFIER=$2
 
 # Constants
-declare TRUE=0
-declare FALSE=1
+declare -r TRUE=0
+declare -r FALSE=1
 
 # External Parameters (maybe load from environment variables of a configuration file)
-declare DEFAULT_GROUP_ID="br.com.miguelmf"
+declare PROJECT_GROUP_ID
 declare ARCHETYPE_ARTIFACT_ID="quickstart-junit5-archetype"
 declare ARCHETYPE_GROUP_ID="br.com.miguelmf"
 
 # Others
-declare USAGE_MESSAGE="
+declare -r USAGE_MESSAGE="
 USAGE: $(basename $0) <ARTIFACT_ID> <PACKAGE-MODIFIER>
 
   ARTIFACT_ID:      The artifactId of the maven module to be generated
@@ -51,9 +55,14 @@ isValid() {
     return "$isValid";
 }
 
+fechGroupIdFromPom() {
+    PROJECT_GROUP_ID=$(grep -oPm1 "(?<=<groupId>)[^<]+" pom.xml)
+}
+
+
 module() {
     mvn archetype:generate                               \
-      -DgroupId=$DEFAULT_GROUP_ID                        \
+      -DgroupId=$PROJECT_GROUP_ID                        \
       -DartifactId="$ARTIFACT_ID"                        \
       -Dpackage="$DEFAULT_PACKAGE"."$PACKAGE_MODIFIER"   \
       -DarchetypeGroupId=$ARCHETYPE_GROUP_ID             \
@@ -64,8 +73,8 @@ module() {
 confirmation() {
     echo "Maven Execution Parameters: "
     echo "    Artifact Id           : $ARTIFACT_ID"
-    echo "    Group Id              : $DEFAULT_GROUP_ID"
-    echo "    Package               : $DEFAULT_GROUP_ID.$PACKAGE_MODIFIER"
+    echo "    Group Id              : $PROJECT_GROUP_ID"
+    echo "    Package               : $PROJECT_GROUP_ID.$PACKAGE_MODIFIER"
     echo "    Archetype Artifact Id : $ARCHETYPE_ARTIFACT_ID"
     echo "    Archetype Group Id    : $ARCHETYPE_GROUP_ID"
     echo ""
@@ -86,6 +95,8 @@ help() {
 }
 
 main() {
+    fechGroupIdFromPom
+
     if isValid; then
         confirmation
     else
